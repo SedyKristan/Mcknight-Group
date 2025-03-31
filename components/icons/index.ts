1890c7ef48d37782f512daa8edf3c9722ef76987
Sedy Kristan <ribayasedy@gmail.com>
2025-03-31 16:30:05 +0800

    feature: update about us icons

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -2,3 +2,7 @@ export { Solid as BurgerMenuSolid } from "./burger-menu";
 export { Solid as XCloseSolid } from "./x-close";
 export { Solid as HouseSolid } from "./house";
 export { Solid as ChevronDownSolid } from "./chevron-down";
+export { Solid as RulerLSolid } from "./ruler-l";
+export { Solid as UsersSolid } from "./users";
+export { Solid as LocationSolid } from "./location";
+export { Solid as BillSolid } from "./bill";
--- slices/About/components/Card.tsx
+++ slices/About/components/Card.tsx
@@ -1,4 +1,10 @@
-import { HouseSolid } from "@/components/icons";
+import {
+  HouseSolid,
+  RulerLSolid,
+  BillSolid,
+  LocationSolid,
+  UsersSolid,
+} from "@/components/icons";
 import { Simplify } from "@/prismicio-types";
 import { Content } from "@prismicio/client";
 
@@ -9,8 +15,18 @@ const Card = ({
 }) => {
   return (
     <div className="flex lg:h-auto w-[372px]  lg:w-full text-[var(--text-primary-dark)] h-[126px] py-4 px-8 lg:py-4 lg:px-8 items-center gap-8 rounded-xl bg-[var(--color-primary-light)] shadow-[var(--drop-shadow-sm)]">
-      <HouseSolid className="h-10 w-10" />
-      <div className="h-full w-[2px] bg-[var(--divider-dark-default)]"></div>
+      {card.icon === "house" ? (
+        <HouseSolid className="h-10 w-10" />
+      ) : card.icon === "ruler-l" ? (
+        <RulerLSolid className="h-10 w-10" />
+      ) : card.icon === "users" ? (
+        <UsersSolid className="h-10 w-10" />
+      ) : card.icon === "location" ? (
+        <LocationSolid className="h-10 w-10" />
+      ) : card.icon === "bill" ? (
+        <BillSolid className="h-10 w-10" />
+      ) : null}
+      <div className="self-stretch w-[2px] bg-[var(--divider-dark-default)]"></div>
       <div className="flex flex-col flex-1">
         <h2>{card.label}</h2>
         <p className="w-full whitespace-normal">{card.details}</p>
